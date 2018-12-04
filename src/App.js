fe420bde578f8be3f8974e8e3e0bdc4799c4d0bc
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-03 23:53:34 -0800

    Linked main stylesheet and modified class name

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from "react";
-import "./styles/css/global.css";
+import Header from "./components/Header";
+import "./styles/css/main.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <div className="block-wrapper">
-            <p className="block">I am a block</p>
-          </div>
-        </header>
+      <div className="app">
+        <Header />
       </div>
     );
   }
