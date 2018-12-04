2d363f86a94d87034dc53fe8f084ffb98f40c6ac
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-03 23:53:12 -0800

    Header Component for header copy created

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <h1>
+        <span className="stem-copy">Stem</span>
+        <span className="reads-copy">reads</span>
+      </h1>
+      <p>A compilation of readings about past and present leaders in STEM.</p>
+    </div>
+  );
+};
+
+export default Header;
