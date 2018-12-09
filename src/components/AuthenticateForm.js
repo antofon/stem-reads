552350cb6294db42d0235e87443bf5df5f4b759b
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-09 11:49:21 -0800

    Style cleanup, added 'Preview Collections' button, account creation link

--- src/components/AuthenticateForm.js
+++ src/components/AuthenticateForm.js
@@ -21,41 +21,50 @@ class AuthenticateForm extends Component {
   // }
   render() {
     return (
-      <div className="form-wrapper">
-        <div class="routing-buttons">
-          <p className="signup-button" onClick={this.handleClick}>
-            Sign up
-          </p>
-          <p className="login-button" onClick={this.handleClick}>
-            Log in
-          </p>
+      <div className="component-wrapper">
+        <div className="form-wrapper">
+          <div className="routing-buttons">
+            <p className="signup-button" onClick={this.handleClick}>
+              Sign up
+            </p>
+            <p className="login-button" onClick={this.handleClick}>
+              Log in
+            </p>
+          </div>
+
+          <form>
+            <div className="input-fields">
+              <div className="username-field">
+                <label htmlFor="username" />
+                Username
+                <input type="text" id="username" />
+              </div>
+
+              <div className="password-field">
+                <label htmlFor="pass" />
+                Password
+                <input type="password" id="pass" />
+              </div>
+            </div>
+
+            <div className="sign-up-buttons">
+              <input type="submit" value="Sign up" />
+              <div className="or-flex">
+                <hr className="horiz-left" />
+                <span className="or-copy">or</span>
+                <hr className="horiz-right" />
+              </div>
+              <input type="submit" value="Use another account" />
+            </div>
+          </form>
         </div>
+        <p className="account-question">
+          <a href="#">Why should I create an account?</a>
+        </p>
 
-        <form>
-          <div className="input-fields">
-            <div className="username-field">
-              <label htmlFor="username" />
-              Username
-              <input type="text" id="username" />
-            </div>
-
-            <div className="password-field">
-              <label htmlFor="pass" />
-              Password
-              <input type="password" id="pass" />
-            </div>
-          </div>
-
-          <div className="sign-up-buttons">
-            <input type="submit" value="Sign up" />
-            <div className="or-flex">
-              <hr className="horiz-left" />
-              <span className="or-copy">or</span>
-              <hr className="horiz-right" />
-            </div>
-            <input type="submit" value="Use another account" />
-          </div>
-        </form>
+        <p className="preview-collection-wrapper">
+          <button className="preview-collection">Preview Collection</button>
+        </p>
       </div>
     );
   }
