0cc90fb3543d006becf9cd94f8defdd93b7dfa2d
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-15 11:58:38 -0800

    Removed comments and whitespace

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,8 @@
 import React from "react";
-import { FaUser } from "react-icons/fa";
+
 const Login = () => {
   return (
-    // <form>
-    //   <div className="input-fields">
-    //     <div className="username-field">
-    //       <label htmlFor="username" />
-    //       Username
-    //       <input type="text" id="login-username" />
-    //     </div>
-
-    //     <div className="password-field">
-    //       <label htmlFor="pass" />
-    //       Password
-    //       <input type="password" id="login-pass" />
-    //     </div>
-    //   </div>
-
-    //   <div className="sign-up-buttons">
-    //     <input type="submit" value="Log in" />
-    //     <div className="or-flex">
-    //       <hr className="horiz-left" />
-    //       <span className="or-copy">or</span>
-    //       <hr className="horiz-right" />
-    //     </div>
-    //     <input type="submit" value="Use another account" />
-    //   </div>
-    // </form>
     <div className="login-wrapper">
-      {/* <p className="login-button">
-        Login <FaUser />
-      </p> */}
       <form>
         <div className="input-fields">
           <div className="username-field">
