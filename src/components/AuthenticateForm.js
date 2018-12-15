db131436a12fe40bdb9aff1e60a6a40fc3576cf5
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-15 11:27:20 -0800

    Fixed Login and Signup button toggling to render respective Components

--- src/components/AuthenticateForm.js
+++ src/components/AuthenticateForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Signup from "./Signup";
 import Login from "./Login";
+import { FaUser } from "react-icons/fa";
 import PreviewCollections from "./PreviewCollections";
 import * as ROUTES from "../constants/routes";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
@@ -31,16 +32,43 @@ class AuthenticateForm extends Component {
     //   authUser: null
     // };
     super(props);
-    this.state = { authUser: false };
-    this.handleClick = this.handleClick.bind(this);
+    this.state = { authUser: true, showAuthSignup: false, showAuthLogin: true };
+    // this.handleClick = this.handleClick.bind(this);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleSignupClick = this.handleSignupClick.bind(this);
   }
 
-  handleClick() {
+  handleSignupClick() {
+    if (this.state.showAuthLogin) {
+      return null;
+    }
     this.setState(state => ({
-      authUser: !state.authUser
+      authUser: !state.authUser,
+      showAuthSignup: !state.showAuthSignup,
+      showAuthLogin: !state.showAuthLogin
     }));
 
-    console.log(`Auth user: ${this.state.authUser}`);
+    console.log(
+      `SignUp Clicked -> Auth signup: ${
+        this.state.showAuthSignup
+      } and Auth login:${this.state.showAuthLogin}`
+    );
+  }
+  handleLoginClick() {
+    if (this.state.showAuthSignup) {
+      return null;
+    }
+    this.setState(state => ({
+      authUser: !state.authUser,
+      showAuthLogin: !state.showAuthLogin,
+      showAuthSignup: !state.showAuthSignup
+    }));
+
+    console.log(
+      `LoginClicked -> Auth signup: ${
+        this.state.showAuthSignup
+      } and Auth login:${this.state.showAuthLogin}`
+    );
   }
 
   render() {
@@ -48,22 +76,42 @@ class AuthenticateForm extends Component {
       <Router>
         <div className="component-wrapper">
           <div className="form-wrapper">
-            <div className="routing-buttons" onClick={this.handleClick}>
-              {this.state.authUser ? <Login /> : <Signup />}
+            <div className="routing-buttons">
+              <p className="signup-button" onClick={this.handleSignupClick}>
+                <span className="auth-copy">Sign up</span>
+                <span className="auth-icon">
+                  <FaUser />
+                </span>
+              </p>
+              <p className="login-button" onClick={this.handleLoginClick}>
+                <span className="auth-copy">Login</span>
+                <span className="auth-icon">
+                  <FaUser />
+                </span>
+              </p>
+            </div>
+            {this.state.authUser ? (
+              <Login authLogin={this.state.showAuthLogin} />
+            ) : (
+              <Signup authSignup={this.state.showAuthSignup} />
+            )}
 
-              {/* <Navigation authUser={this.state.authUser} /> *
+            {/* {this.state.authUser ? <Login /> : <Signup />} */}
+            {/* <Login />
+              <Signup /> */}
+            {/* <Navigation authUser={this.state.authUser} /> *
             </div>
 
             {/* <div className="routing-buttons"> */}
-              {/*<p className="signup-button" onClick={this.handleClick}>
+            {/*<p className="signup-button" onClick={this.handleClick}>
               Sign up
             </p>
             <p className="login-button" onClick={this.handleClick}>
               Log in
     </p>*/}
-              {/* <Navigation authUser={this.state.authUser} /> */}
-              {/* </div> */}
-            </div>
+            {/* <Navigation authUser={this.state.authUser} /> */}
+            {/* </div> */}
+
             <p className="account-question">
               <Link to={ROUTES.PREVIEW_COLLECTIONS}>
                 Why should I create an account?
