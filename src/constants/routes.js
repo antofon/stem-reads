bc66c914f7ea7e2b30aad3b50cc8e5aa40ad9d59
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-15 01:15:55 -0800

    Edited path names for route constants

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,7 +1,8 @@
-export const LANDING = "/";
+export const LANDING = "/home";
 export const SIGN_UP = "/signup";
 export const LOG_IN = "/login";
-export const HOME = "/home";
+export const DASHBOARD = "/dashboard";
 export const COLLECTIONS = "/collections";
 export const ACCOUNT = "/account";
 export const PASSWORD_FORGET = "/pw-forget";
+export const NOT_FOUND = "/:homeId";
