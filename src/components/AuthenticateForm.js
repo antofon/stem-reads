39e46f489f467e6a23f350e17bbb4def5e67ff11
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-15 11:58:05 -0800

    Extracted necessary JSX and placed in AuthenticateForm Component

--- src/components/AuthenticateForm.js
+++ src/components/AuthenticateForm.js
@@ -1,39 +1,14 @@
 import React, { Component } from "react";
 import Signup from "./Signup";
 import Login from "./Login";
-import { FaUser } from "react-icons/fa";
-import PreviewCollections from "./PreviewCollections";
 import * as ROUTES from "../constants/routes";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-// import Navigation from "./Navigation";
+import { Link } from "react-router-dom";
+import { FaUser } from "react-icons/fa";
+
 class AuthenticateForm extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = { clicked: true };
-  //     this.handleClick = this.handleClick.bind(this);
-  //   }
-  //   handleClick() {
-  //     if (this.state.clicked === "true") {
-  //       //   this.setState(state => ({
-
-  //       //   }));
-  //       alert(this.state.clicked);
-  //     } else {
-  //       this.setState(state => ({
-  //         clicked: !state.clicked
-  //       }));
-  //       alert(this.state.clicked);
-  //     }
-  // }
-
   constructor(props) {
-    // super(props);
-    // this.state = {
-    //   authUser: null
-    // };
     super(props);
     this.state = { authUser: true, showAuthSignup: false, showAuthLogin: true };
-    // this.handleClick = this.handleClick.bind(this);
     this.handleLoginClick = this.handleLoginClick.bind(this);
     this.handleSignupClick = this.handleSignupClick.bind(this);
   }
@@ -70,60 +45,47 @@ class AuthenticateForm extends Component {
       } and Auth login:${this.state.showAuthLogin}`
     );
   }
-
   render() {
     return (
-      <Router>
-        <div className="component-wrapper">
-          <div className="form-wrapper">
-            <div className="routing-buttons">
-              <p className="signup-button" onClick={this.handleSignupClick}>
-                <span className="auth-copy">Sign up</span>
-                <span className="auth-icon">
-                  <FaUser />
-                </span>
-              </p>
-              <p className="login-button" onClick={this.handleLoginClick}>
-                <span className="auth-copy">Login</span>
-                <span className="auth-icon">
-                  <FaUser />
-                </span>
-              </p>
-            </div>
-            {this.state.authUser ? (
-              <Login authLogin={this.state.showAuthLogin} />
-            ) : (
-              <Signup authSignup={this.state.showAuthSignup} />
-            )}
-
-            {/* {this.state.authUser ? <Login /> : <Signup />} */}
-            {/* <Login />
-              <Signup /> */}
-            {/* <Navigation authUser={this.state.authUser} /> *
-            </div>
-
-            {/* <div className="routing-buttons"> */}
-            {/*<p className="signup-button" onClick={this.handleClick}>
-              Sign up
+      <div className="component-wrapper">
+        <div className="form-wrapper">
+          <div className="routing-buttons">
+            <p className="signup-button" onClick={this.handleSignupClick}>
+              <span className="auth-copy">Sign up</span>
+              <span className="auth-icon">
+                <FaUser />
+              </span>
             </p>
-            <p className="login-button" onClick={this.handleClick}>
-              Log in
-    </p>*/}
-            {/* <Navigation authUser={this.state.authUser} /> */}
-            {/* </div> */}
-
-            <p className="account-question">
-              <Link to={ROUTES.PREVIEW_COLLECTIONS}>
-                Why should I create an account?
-              </Link>
+            <p className="login-button" onClick={this.handleLoginClick}>
+              <span className="auth-copy">Login</span>
+              <span className="auth-icon">
+                <FaUser />
+              </span>
             </p>
-            <Route
-              path={ROUTES.PREVIEW_COLLECTIONS}
-              component={PreviewCollections}
-            />
           </div>
+          {this.state.authUser ? (
+            <Login authLogin={this.state.showAuthLogin} />
+          ) : (
+            <Signup authSignup={this.state.showAuthSignup} />
+          )}
+
+          {/* {this.state.authUser ? <Login /> : <Signup />} */}
+          {/* <Login />
+            <Signup /> */}
+          {/* <Navigation authUser={this.state.authUser} /> *
+          </div>
+
+          {/* <div className="routing-buttons"> */}
+
+          {/* <Navigation authUser={this.state.authUser} /> */}
+          {/* </div> */}
         </div>
-      </Router>
+        <p className="account-question">
+          <Link to={ROUTES.PREVIEW_COLLECTIONS}>
+            Why should I create an account?
+          </Link>
+        </p>
+      </div>
     );
   }
 }
