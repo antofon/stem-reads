ef59bb172cb6cb73a56fdde3ba347635e3365bba
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-16 20:51:30 -0800

    Converted Stateless Functional Component to regular Component

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Signup = () => {
-  return (
-    <div className="signup-wrapper">
-      <form>
-        <div className="input-fields">
-          <div className="username-field">
-            <label htmlFor="username" />
-            Username
-            <input type="text" id="username" />
-          </div>
-          <div className="password-field">
-            <label htmlFor="pass" />
-            Password
-            <input type="password" id="pass" />
-          </div>
-        </div>
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+  }
 
-        <div className="sign-up-buttons">
-          <input type="submit" value="Sign up" />
-          <div className="or-flex">
-            <hr className="horiz-left" />
-            <span className="or-copy">or</span>
-            <hr className="horiz-right" />
+  onSubmit = event => {};
+
+  render() {
+    return (
+      <div className="signup-wrapper">
+        <form>
+          <div className="input-fields">
+            <div className="username-field">
+              <label htmlFor="username" />
+              Username
+              <input type="text" id="username" />
+            </div>
+            <div className="password-field">
+              <label htmlFor="pass" />
+              Password
+              <input type="password" id="pass" />
+            </div>
+
+            <div className="confirm-password-field">
+              <label htmlFor="confirm-pass" />
+              Confirm Password
+              <input type="password" id="confirm-pass" />
+            </div>
           </div>
-          <input type="submit" value="Use another account" />
-        </div>
-      </form>
-    </div>
-  );
-};
+
+          <div className="sign-up-buttons">
+            <input type="submit" value="Sign up" onSubmit={this.onSubmit} />
+            <div className="or-flex">
+              <hr className="horiz-left" />
+              <span className="or-copy">or</span>
+              <hr className="horiz-right" />
+            </div>
+            <input type="submit" value="Use another account" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Signup;
