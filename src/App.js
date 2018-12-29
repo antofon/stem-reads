515b7677c5ef8b39b9e1c5f89077b534c24136a4
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-28 20:10:55 -0800

    Moved routing into App file, always render Header Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import "./styles/css/main.css";
+import NotFound from "./components/NotFound";
+import Dashboard from "./components/Dashboard";
+import AccountFaq from "./components/AccountFaq";
+import PreviewCollections from "./components/PreviewCollections";
 
 class App extends Component {
   render() {
     return (
-      <div className="app">
-        <Header />
-        <Landing />
-      </div>
+      <Router>
+        <div>
+          <div className="app">
+            <Header />
+          </div>
+          <Switch>
+            <Route
+              exact
+              path={ROUTES.LANDING}
+              render={props => (
+                // <div className="app">
+                <Landing />
+                // {/* <AuthenticateForm /> */}
+                // </div>
+              )}
+            />
+            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
+            <Route path={ROUTES.ACCOUNT_FAQ} component={AccountFaq} />
+            <Route
+              path={ROUTES.PREVIEW_COLLECTIONS}
+              component={PreviewCollections}
+            />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
