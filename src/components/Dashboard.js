68a89b6053129f3f0831ce514b668166ffcc9430
Anwana Ntofon <anwanantofon@gmail.com>
2018-12-30 02:55:53 -0800

    Experimenting with sign out authentication, sign out button included but not working

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { FirebaseContext } from "./Firebase";
+import * as ROUTES from "../constants/routes";
 
-const Dashboard = () => (
-  <div>
-    <h2>Welcome to the Dashboard!!!!</h2>
-  </div>
-);
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Welcome to the Dashboard!!!!</h2>
+        <FirebaseContext.Consumer>
+          {firebase => (
+            <button
+              type="submit"
+              firebase={firebase}
+              id="Sign out"
+              onClick={() => {
+                firebase.doSignOut(() =>
+                  this.props.history.push(ROUTES.LANDING)
+                );
+              }}
+            >
+              Sign out
+            </button>
+          )}
+        </FirebaseContext.Consumer>
+      </div>
+    );
+  }
+}
 
-export default Dashboard;
+export default withRouter(Dashboard);
