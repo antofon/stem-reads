1e860e0eb4eec2bd219e9854670c643c97410d9b
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-16 01:28:40 -0800

    Implemented function that determines if user is logged in or not, render component and routes accordingly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import app from "./Firebase/firebase";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -11,33 +12,64 @@ import PreviewCollections from "./components/PreviewCollections";
 import SignupSuccess from "./components/Auth/SignupSuccess";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {}
+    };
+  }
+
+  componentDidMount() {
+    //when component is first mounted to page (rendered for the first time)
+    this.authListener();
+  }
+
+  // checks if the authentication state of a user changes (i.e. logged in)
+  authListener() {
+    app.auth().onAuthStateChanged(user => {
+      if (user) {
+        // user is currently [object Object]. can wrap this in JSON.stringify() to break down object and see key-value pairs
+        console.log(`User logged in: ${user}`);
+        this.setState({ user });
+        //prints potential user name of logged in user
+        console.log(app.auth().currentUser.displayName);
+        //localStorage.setItem('user', user.uid);
+      } else {
+        console.log(`User not logged in: ${user}`);
+        this.setState({ user: null });
+        //localStorage.setItem('user', user.uid);
+      }
+    });
+  }
   render() {
     return (
       <Router>
         <div>
           <div className="app">
             <Header />
+            {/* check if user is logged in or not, protecting certain routes from being accessed */}
+            {this.state.user ? (
+              <Switch>
+                <Route exact path={ROUTES.DASHBOARD} component={Dashboard} />
+                <Route path={ROUTES.ACCOUNT_FAQ} component={AccountFaq} />
+                <Route
+                  path={ROUTES.SIGN_UP_SUCCESS}
+                  component={SignupSuccess}
+                />
+                <Route
+                  path={ROUTES.PREVIEW_COLLECTIONS}
+                  component={PreviewCollections}
+                />
+                <Route component={NotFound} />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route exact path={ROUTES.LANDING} component={Landing} />
+                <Route path={ROUTES.ACCOUNT_FAQ} component={AccountFaq} />
+                <Route component={NotFound} />
+              </Switch>
+            )}
           </div>
-          <Switch>
-            <Route
-              exact
-              path={ROUTES.LANDING}
-              render={props => (
-                // <div className="app">
-                <Landing />
-                // {/* <AuthenticateForm /> */}
-                // </div>
-              )}
-            />
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} />
-            <Route path={ROUTES.ACCOUNT_FAQ} component={AccountFaq} />
-            <Route path={ROUTES.SIGN_UP} component={SignupSuccess} />
-            <Route
-              path={ROUTES.PREVIEW_COLLECTIONS}
-              component={PreviewCollections}
-            />
-            <Route component={NotFound} />
-          </Switch>
         </div>
       </Router>
     );
