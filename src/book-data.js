1a5de357f1170ba80c6bc0e11f3b8544172c6ab7
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-19 17:49:06 -0800

    Created location for book data

---
+++ src/book-data.js
@@ -0,0 +1,127 @@
+const books = {
+  book1: {
+    id: "1",
+    author: "Duncan Clark",
+    title: "Alibaba. The House That Jack Ma Built",
+    type: "non-fiction",
+    image: "../public/images/alibaba_the_house_that_jack_ma_built.jpg"
+  },
+
+  book2: {
+    id: "2",
+    author: "Kevin Mitnick with William L. Simon",
+    title: "Ghost in the Wires",
+    type: "non-fiction",
+    image: "../public/images/ghost_in_the_wires.jpg"
+  },
+
+  book3: {
+    id: "3",
+    author: "Kurt W. Beyer",
+    title: "Grace Hopper, and the Invention of the Information Age",
+    type: "non-fiction",
+    image:
+      "../public/images/grace_hopper_and_the_invention_of_the_information_age.jpg"
+  },
+
+  book4: {
+    id: "4",
+    author: "Margot Lee Shetterly",
+    title: "Hidden Figures",
+    type: "non-fiction",
+    image: "../public/images/hidden_figures.jpg"
+  },
+
+  book5: {
+    id: "5",
+    author: "Roda Ahmed",
+    title: "Mae Among The Stars",
+    type: "illustrated",
+    image: "../public/images/mae_among_the_stars.jpg"
+  },
+
+  book6: {
+    id: "6",
+    author: "Rachel Ignotofsky",
+    title: "Women in Science, 50 Fearless Pioneers who Changed the World",
+    type: "illustrated",
+    image: "../public/images/women_in_science_pioneers.jpg"
+  },
+
+  book7: {
+    id: "7",
+    author: "Julia Finley Mosca",
+    title: "The Doctor with an Eye for Eyes. The Story of Dr. Patricia Bath",
+    type: "illustrated",
+    image: "../public/images/the_doctor_with_an_eye_for_eyes.jpg"
+  },
+
+  book8: {
+    id: "8",
+    author: "Julia Finley Mosca",
+    title: "The Girl with a Mind for Math. The Story of Raye Montague.",
+    type: "illustrated",
+    image: "../public/images/the_girl_with_a_mind_for_math.jpg"
+  }
+};
+
+export default books;
+//   book2: {
+//     id: "2",
+//     author: "Richard L. Brandt",
+//     title: "one click. jeff bezos and the rise of amazon.com",
+//     type: "non-fiction",
+//     image: "../public/images/"
+//   },
+//   book3: {
+//     id: "3",
+//     author: "Georgina Ferry",
+//     title: "Dorothy Hodgkin, A Life",
+//     type: "non-fiction",
+//     image: "../public/images/"
+//   },
+//   book7: {
+//     id: "7",
+//     author: "Sharon Bertsch McGrayne",
+//     title: "Nobel Prize Women in Science",
+//     type: "non-fiction",
+//     image: "../public/images/"
+//   },
+//   book8: {
+//     id: "8",
+//     author: "Justin Peters",
+//     title:
+//       "The Idealist. Aaron Swartz and the Rise of Free Culture on The Internet",
+//     type: "non-fiction",
+//     image: "../public/images/"
+//   },
+//   book9: {
+//     id: "9",
+//     author: "Diane Stanley",
+//     title: "Ada Lovelace, Poet of Science",
+//     type: "illustrated",
+//     image: "../public/images/"
+//   },
+//   book10: {
+//     id: "10",
+//     author: "Emily Arnold McCully",
+//     title: "Caroline's Comets, A True Story",
+//     type: "illustrated",
+//     image: "../public/images/"
+//   },
+//   book12: {
+//     id: "12",
+//     author: "Jess Keating",
+//     title:
+//       "Shark Lady. The True Story of How Eugenie Clark Became the Ocean's Most Fearless Scientist",
+//     type: "illustrated",
+//     image: "../public/images/"
+//   },
+//   book14: {
+//     id: "14",
+//     author: "Dean Robbins",
+//     title:
+//       "Margaret and the Moon, How Margaret Hamilton Saved the First Lunar Landing",
+//     type: "illustrated",
+//     image: "../public/images/"
+//   },
