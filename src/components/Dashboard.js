e2d54d214d3d260b15c1e663bd51e3924e511655
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-19 23:54:51 -0800

    Successfully loop through Book Data by passing down props, and commented out some code

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 import app from "../Firebase/firebase";
 import BookList from "./BookList";
+import Book from "./Book";
 import * as ROUTES from "../constants/routes";
 
 class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      type: "non-fiction"
+    };
+  }
   signOut = () => {
     console.log("User signed out successfully");
     app.auth().signOut();
@@ -15,13 +23,16 @@ class Dashboard extends Component {
     return (
       <div>
         <h2>Welcome to the Dashboard!!!!</h2>
-
-        <button onClick={this.props.renderBooks}>Click Me</button>
+        {/* {this.props.loadBooks} */}
+        {/* <button onClick={this.props.loadBooks}>Load Books</button> */}
         <div className="books-wrapper" />
         <div>Autobiography &amp; Non-Fiction</div>
-        <BookList type="non-fiction" />
+        {Object.keys(this.props.books).map(key => (
+          <Book key={key} index={key} details={this.props.books[key]} />
+        ))}
+        {/* <BookList books={this.state.books} type={this.state.type} /> */}
         <div>Fiction</div>
-        <BookList type="fiction" />
+        {/* <BookList books={this.state.books} type={this.state.type} /> */}
         <input
           type="button"
           value="Sign out"
