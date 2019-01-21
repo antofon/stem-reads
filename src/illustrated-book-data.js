47f64734c435ff7926c82ebf9c6065db712967ba
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-20 18:15:40 -0800

    Split book data into two files for each genre

---
+++ src/illustrated-book-data.js
@@ -0,0 +1,35 @@
+const illustratedBooks = {
+  book5: {
+    id: "5",
+    author: "Roda Ahmed",
+    title: "Mae Among The Stars",
+    type: "illustrated",
+    image: "/images/illustrated/mae_among_the_stars.jpg"
+  },
+
+  book6: {
+    id: "6",
+    author: "Rachel Ignotofsky",
+    title: "Women in Science, 50 Fearless Pioneers who Changed the World",
+    type: "illustrated",
+    image: "/images/illustrated/women_in_science_pioneers.jpg"
+  },
+
+  book7: {
+    id: "7",
+    author: "Julia Finley Mosca",
+    title: "The Doctor with an Eye for Eyes. The Story of Dr. Patricia Bath",
+    type: "illustrated",
+    image: "/images/illustrated/the_doctor_with_an_eye_for_eyes.jpg"
+  },
+
+  book8: {
+    id: "8",
+    author: "Julia Finley Mosca",
+    title: "The Girl with a Mind for Math. The Story of Raye Montague.",
+    type: "illustrated",
+    image: "/images/illustrated/the_girl_with_a_mind_for_math.jpg"
+  }
+};
+
+export default illustratedBooks;
--- src/real-book-data.js
+++ src/real-book-data.js
@@ -1,4 +1,4 @@
-const books = {
+const realBooks = {
   book1: {
     id: "1",
     author: "Duncan Clark",
@@ -30,42 +30,43 @@ const books = {
     title: "Hidden Figures",
     type: "non-fiction",
     image: "/images/real/hidden_figures.jpg"
-  },
-
-  book5: {
-    id: "5",
-    author: "Roda Ahmed",
-    title: "Mae Among The Stars",
-    type: "illustrated",
-    image: "/images/illustrated/mae_among_the_stars.jpg"
-  },
-
-  book6: {
-    id: "6",
-    author: "Rachel Ignotofsky",
-    title: "Women in Science, 50 Fearless Pioneers who Changed the World",
-    type: "illustrated",
-    image: "/images/illustrated/women_in_science_pioneers.jpg"
-  },
-
-  book7: {
-    id: "7",
-    author: "Julia Finley Mosca",
-    title: "The Doctor with an Eye for Eyes. The Story of Dr. Patricia Bath",
-    type: "illustrated",
-    image: "/images/illustrated/the_doctor_with_an_eye_for_eyes.jpg"
-  },
-
-  book8: {
-    id: "8",
-    author: "Julia Finley Mosca",
-    title: "The Girl with a Mind for Math. The Story of Raye Montague.",
-    type: "illustrated",
-    image: "/images/illustrated/the_girl_with_a_mind_for_math.jpg"
   }
 };
 
-export default books;
+export default realBooks;
+// book5: {
+//   id: "5",
+//   author: "Roda Ahmed",
+//   title: "Mae Among The Stars",
+//   type: "illustrated",
+//   image: "/images/illustrated/mae_among_the_stars.jpg"
+// },
+
+// book6: {
+//   id: "6",
+//   author: "Rachel Ignotofsky",
+//   title: "Women in Science, 50 Fearless Pioneers who Changed the World",
+//   type: "illustrated",
+//   image: "/images/illustrated/women_in_science_pioneers.jpg"
+// },
+
+// book7: {
+//   id: "7",
+//   author: "Julia Finley Mosca",
+//   title: "The Doctor with an Eye for Eyes. The Story of Dr. Patricia Bath",
+//   type: "illustrated",
+//   image: "/images/illustrated/the_doctor_with_an_eye_for_eyes.jpg"
+// },
+
+// book8: {
+//   id: "8",
+//   author: "Julia Finley Mosca",
+//   title: "The Girl with a Mind for Math. The Story of Raye Montague.",
+//   type: "illustrated",
+//   image: "/images/illustrated/the_girl_with_a_mind_for_math.jpg"
+// }
+// };
+
 //   book2: {
 //     id: "2",
 //     author: "Richard L. Brandt",
