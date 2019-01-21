bb62b6f958ec1b3a0ab1dfd61fa90251742d49ac
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-20 18:19:55 -0800

    Removed comments of old code

--- src/components/Book.js
+++ src/components/Book.js
@@ -11,19 +11,6 @@ class Book extends Component {
           <p>{author}</p>
         </figcaption>
       </figure>
-      //   <li>I'm a Book</li>
-      //   {/* <li>{this.props.books}</li> */}
-      //   {/* {this.state.books.map(books => (
-      //     <li key={books.id}>
-      //       <figure>
-      //         <img src={books.image} alt={books.title} />
-      //         <figcaption>
-      //           <div>{books.title}</div>
-      //           <div>{books.author}</div>
-      //         </figcaption>
-      //       </figure>
-      //     </li>
-      //   ))} */}
     );
   }
 }
