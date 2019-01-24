d11d907fa60d95db83af72e0dcb604ba4441ee50
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-23 22:28:40 -0800

    Created file for all of book data

---
+++ src/book-data.js
@@ -0,0 +1,68 @@
+const books = {
+  book1: {
+    id: "1",
+    author: "Duncan Clark",
+    title: "Alibaba. The House That Jack Ma Built",
+    type: "real",
+    image: "/images/real/alibaba_the_house_that_jack_ma_built.jpg"
+  },
+
+  book2: {
+    id: "2",
+    author: "Kevin Mitnick with William L. Simon",
+    title: "Ghost in the Wires",
+    type: "real",
+    image: "/images/real/ghost_in_the_wires.jpg"
+  },
+
+  book3: {
+    id: "3",
+    author: "Kurt W. Beyer",
+    title: "Grace Hopper, and the Invention of the Information Age",
+    type: "real",
+    image:
+      "/images/real/grace_hopper_and_the_invention_of_the_information_age.jpg"
+  },
+
+  book4: {
+    id: "4",
+    author: "Margot Lee Shetterly",
+    title: "Hidden Figures",
+    type: "real",
+    image: "/images/real/hidden_figures.jpg"
+  },
+
+  book5: {
+    id: "5",
+    author: "Roda Ahmed",
+    title: "Mae Among The Stars",
+    type: "illustrated",
+    image: "/images/illustrated/mae_among_the_stars.jpg"
+  },
+
+  book6: {
+    id: "6",
+    author: "Rachel Ignotofsky",
+    title: "Women in Science, 50 Fearless Pioneers who Changed the World",
+    type: "illustrated",
+    image: "/images/illustrated/women_in_science_pioneers.jpg"
+  },
+
+  book7: {
+    id: "7",
+    author: "Julia Finley Mosca",
+    title: "The Doctor with an Eye for Eyes. The Story of Dr. Patricia Bath",
+    type: "illustrated",
+    image: "/images/illustrated/the_doctor_with_an_eye_for_eyes.jpg"
+  },
+
+  book8: {
+    id: "8",
+    author: "Julia Finley Mosca",
+    title: "The Girl with a Mind for Math. The Story of Raye Montague.",
+    type: "illustrated",
+    image: "/images/illustrated/the_girl_with_a_mind_for_math.jpg"
+  }
+};
+
+export default books;
