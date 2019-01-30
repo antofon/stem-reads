dd9d23d73d7052b3d462ab0a74345169ee822bf7
Anwana Ntofon <anwanantofon@gmail.com>
2019-01-29 22:13:56 -0800

    Refactored into Stateless Functional Component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Book extends Component {
-  render() {
-    const { author, title, image } = this.props.details;
-    return (
-      <figure>
-        <img className="book-image" src={image} alt={title} />
-        <figcaption>
-          <p className="book-title">{title}</p>
-          <p className="book-author">{author}</p>
-        </figcaption>
-      </figure>
-    );
-  }
-}
+const Book = props => {
+  const { author, title, image } = props.details;
+
+  return (
+    <figure>
+      <img className="book-image" src={image} alt={title} />
+      <figcaption>
+        <p className="book-title">{title}</p>
+        <p className="book-author">{author}</p>
+      </figcaption>
+    </figure>
+  );
+};
 
 export default Book;
