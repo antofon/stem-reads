4ff800c9dc2429171ac5bfb57941d19124ec3117
Anwana Ntofon <anwanantofon@gmail.com>
2019-02-03 12:22:53 -0800

    Renaming props, removed Book genre title, sign out button moved next to load and clear book buttons

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -24,20 +24,36 @@ class Dashboard extends Component {
   render() {
     return (
       <div>
-        {/* <h2>Welcome to the Dashboard!!!!</h2> */}
-        <button onClick={this.props.loadBooks}>Load Books</button>
-        <button onClick={this.props.removeBooks}>Remove Books</button>
-        <div className="books-wrapper">
-          <div>
-            <BookList genres={this.state.genres} books={this.props.books} />
-          </div>
-        </div>
         <input
           type="button"
           value="Sign out"
           id="Sign out"
           onClick={this.signOut}
         />
+        {/* <h2>Welcome to the Dashboard!!!!</h2> */}
+        <button onClick={this.props.loadBooks}>Load Books</button>
+        <button onClick={this.props.clearBooks}>Clear Books</button>
+        <div className="books-wrapper">
+          <div>
+            <div>
+              <h3>{this.props.username}'s Dashboard</h3>
+              {/* <h3 className="non-fiction">Autobiography &amp; Non-Fiction</h3> */}
+              <BookList
+                type1={this.state.genres.type1}
+                deleteBook={this.props.deleteBook}
+                books={this.props.books}
+              />
+            </div>
+            <div>
+              {/* <h3 className="illustrated">Illustrated</h3> */}
+              <BookList
+                type2={this.state.genres.type2}
+                deleteBook={this.props.deleteBook}
+                books={this.props.books}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
