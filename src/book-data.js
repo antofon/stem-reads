b22f781257de28acb5f0d72949fe2d4e428ff13f
Anwana Ntofon <anwanantofon@gmail.com>
2019-02-09 20:38:25 -0800

    Added Amazon.com links for each Book

--- src/book-data.js
+++ src/book-data.js
@@ -4,7 +4,8 @@ const books = {
     author: "Duncan Clark",
     title: "Alibaba. The House That Jack Ma Built",
     type: "real",
-    image: "/images/real/alibaba_the_house_that_jack_ma_built.jpg"
+    image: "/images/real/alibaba_the_house_that_jack_ma_built.jpg",
+    link: "https://www.amazon.com/Alibaba-House-That-Jack-Built/dp/0062413414/"
   },
 
   book2: {
@@ -12,7 +13,9 @@ const books = {
     author: "Kevin Mitnick with William L. Simon",
     title: "Ghost in the Wires",
     type: "real",
-    image: "/images/real/ghost_in_the_wires.jpg"
+    image: "/images/real/ghost_in_the_wires.jpg",
+    link:
+      "https://www.amazon.com/Ghost-Wires-Adventures-Worlds-Wanted/dp/0316037729/"
   },
 
   book3: {
@@ -21,7 +24,9 @@ const books = {
     title: "Grace Hopper, and the Invention of the Information Age",
     type: "real",
     image:
-      "/images/real/grace_hopper_and_the_invention_of_the_information_age.jpg"
+      "/images/real/grace_hopper_and_the_invention_of_the_information_age.jpg",
+    link:
+      "https://www.amazon.com/Invention-Information-Lemelson-Studies-Innovation/dp/0262517264/"
   },
 
   book4: {
@@ -29,7 +34,9 @@ const books = {
     author: "Margot Lee Shetterly",
     title: "Hidden Figures",
     type: "real",
-    image: "/images/real/hidden_figures.jpg"
+    image: "/images/real/hidden_figures.jpg",
+    link:
+      "https://www.amazon.com/Hidden-Figures-American-Untold-Mathematicians/dp/0062363603/"
   },
 
   book5: {
@@ -37,7 +44,8 @@ const books = {
     author: "Roda Ahmed",
     title: "Mae Among The Stars",
     type: "illustrated",
-    image: "/images/illustrated/mae_among_the_stars.jpg"
+    image: "/images/illustrated/mae_among_the_stars.jpg",
+    link: "https://www.amazon.com/Mae-Among-Stars-Roda-Ahmed/dp/0062651730/"
   },
 
   book6: {
@@ -45,7 +53,9 @@ const books = {
     author: "Rachel Ignotofsky",
     title: "Women in Science, 50 Fearless Pioneers who Changed the World",
     type: "illustrated",
-    image: "/images/illustrated/women_in_science_pioneers.jpg"
+    image: "/images/illustrated/women_in_science_pioneers.jpg",
+    link:
+      "https://www.amazon.com/Women-Science-Fearless-Pioneers-Changed/dp/1607749769/"
   },
 
   book7: {
@@ -53,7 +63,9 @@ const books = {
     author: "Julia Finley Mosca",
     title: "The Doctor with an Eye for Eyes. The Story of Dr. Patricia Bath",
     type: "illustrated",
-    image: "/images/illustrated/the_doctor_with_an_eye_for_eyes.jpg"
+    image: "/images/illustrated/the_doctor_with_an_eye_for_eyes.jpg",
+    link:
+      "https://www.amazon.com/Doctor-Eye-Eyes-Patricia-Scientists/dp/1943147310/"
   },
 
   book8: {
@@ -61,7 +73,9 @@ const books = {
     author: "Julia Finley Mosca",
     title: "The Girl with a Mind for Math. The Story of Raye Montague.",
     type: "illustrated",
-    image: "/images/illustrated/the_girl_with_a_mind_for_math.jpg"
+    image: "/images/illustrated/the_girl_with_a_mind_for_math.jpg",
+    link:
+      "https://www.amazon.com/Girl-Mind-Math-Montague-Scientists/dp/1943147426/"
   }
 };
 
--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,27 @@
 import React from "react";
 
 const Book = props => {
-  const { author, title, image } = props.details;
+  const { author, title, image, link } = props.details;
 
   return (
-    <figure>
-      <img className="book-image" src={image} alt={title} />
-      <figcaption>
-        <p className="book-title">{title}</p>
-        <p className="book-author">{author}</p>
-      </figcaption>
-      <button onClick={() => props.deleteBook(props.index)}>Delete Book</button>
-    </figure>
+    <div>
+      <figure>
+        <img className="book-image" src={image} alt={title} />
+        <figcaption>
+          <p className="book-title">{title}</p>
+          <p className="book-author">{author}</p>
+        </figcaption>
+      </figure>
+
+      <div className="book-buttons">
+        <a href={link} target="_blank" rel="noopener noreferrer">
+          View on Amazon
+        </a>
+        <button onClick={() => props.deleteBook(props.index)}>
+          Delete Book
+        </button>
+      </div>
+    </div>
   );
 };
-
 export default Book;
