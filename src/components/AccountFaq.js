9eeb228b8201e0f32bc93724ff85c1217129a390
Anwana Ntofon <anwanantofon@gmail.com>
2019-02-20 23:39:35 -0800

    Completed Account FAQ page, structure and styling (including responsiveness)

--- src/components/AccountFaq.js
+++ src/components/AccountFaq.js
@@ -1,9 +1,30 @@
 import React from "react";
+import * as ROUTES from "../constants/routes";
+import { Link } from "react-router-dom";
 
-const AccountFaq = () => (
-  <div>
-    <h2>Account FAQ!!!!</h2>
-  </div>
-);
+const AccountFaq = () => {
+  return (
+    <div className="faq-explanation-wrapper">
+      <h2 className="faq-title">Account FAQ</h2>
+
+      <div className="faq-explanation-body">
+        <p className="faq-explanation">
+          Creating an account allows you to maintain real-time records of your
+          books and other account information. Your user credentials are solely
+          used for getting started and logging into the app.
+        </p>
+
+        <p className="faq-explanation">
+          You can even create a new email that is not connected to your primary
+          email address!
+        </p>
+      </div>
+
+      <Link to={ROUTES.LANDING} className="route">
+        <button className="return-home">Return to Homepage</button>
+      </Link>
+    </div>
+  );
+};
 
 export default AccountFaq;
