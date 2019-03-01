81231f9555f6240b71971d5e0582513e4e9da17b
Anwana Ntofon <anwanantofon@gmail.com>
2019-02-28 22:19:25 -0800

    Created match condition to show or hide landing page subcopy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,35 @@
 import React from "react";
+import { matchPath } from "react-router";
 
-const Header = () => {
+const Header = props => {
+  // want to return match with data or no match
+  const match = !matchPath(props.pathname, {
+    path: "/",
+    exact: true,
+    strict: false
+  });
   return (
     <div className="header-wrapper">
       <h1>
         <span className="stem-copy">Stem</span>
         <span className="reads-copy">reads</span>
       </h1>
-      {/*<p>A collection of readings about past and present leaders in STEM.</p>*/}
+      {/* want to show/hide sub copy based on if user is logged in our out and there is not a match or paths */}
+      {(props.userAuth || !props.userAuth) && match ? null : (
+        <p>
+          Explore illustrated and fictional books about past and present
+          pioneers in&nbsp;
+          <a
+            href="https://en.wikipedia.org/wiki/Science,_technology,_engineering,_and_mathematics"
+            title="Science, Technology, Engineering, Mathematics"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            STEM
+          </a>
+          .
+        </p>
+      )}
     </div>
   );
 };
